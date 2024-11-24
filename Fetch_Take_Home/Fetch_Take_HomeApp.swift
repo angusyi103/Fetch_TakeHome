ef23891c240142c007d2d98abdf17f982b90f4e3
angusyi103 <angusyi103@gmail.com>
2024-11-24 12:43:06 -0700

    add project base

---
+++ Fetch_Take_Home/Fetch_Take_HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_Take_HomeApp.swift
+//  Fetch_Take_Home
+//
+//  Created by angusyi on 11/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_Take_HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Fetch_Take_HomeTests/Fetch_Take_HomeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_Take_HomeTests.swift
+//  Fetch_Take_HomeTests
+//
+//  Created by angusyi on 11/21/24.
+//
+
+import Testing
+@testable import Fetch_Take_Home
+
+struct Fetch_Take_HomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
